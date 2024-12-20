fix accuracy metric calculation

data_setup.py
@@ -15,7 +15,7 @@ class CustomDataset(Dataset):
         x = self.X.iloc[index]
         y = self.target.iloc[index]
 
-        return torch.tensor(x, dtype=torch.float32).to(device), torch.tensor(y, dtype=torch.long)
+        return torch.tensor(x, dtype=torch.float32).to(device), torch.tensor(y, dtype=torch.long).to(device)
 
     def __len__(self):
         return len(self.df)

engine.py
@@ -4,27 +4,34 @@ from tqdm import tqdm
 from utils import device
 
 
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+
 def train_step(model: torch.nn.Module,
                dataloader: torch.utils.data.DataLoader,
                loss_fn: torch.nn.Module,
                optimizer: torch.optim.Optimizer,
                device: torch.device) -> Tuple[float, float]:
 
+    model.train()
     train_loss, train_acc = 0, 0
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
-        model.train()
+
         y_pred = model(X)
 
-        loss = loss_fn(y, y_pred)
+        loss = loss_fn(y_pred, y)
         train_loss += loss.item()
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
-        train_acc += (y_pred_class == y)/sum().item() / len(y_pred)
+        y_pred_labels = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += accuracy_fn(y, y_pred_labels)
 
     train_loss = train_loss / len(dataloader)
     train_acc = train_acc / len(dataloader)
@@ -42,12 +49,12 @@ def test_step(model: torch.nn.Module,
         for batch, (X, y) in enumerate(dataloader):
             X, y = X.to(device), y.to(device)
 
-            y_pred_logits = model(X)
-            loss = loss_fn(y, y_pred_logits)
+            y_pred = model(X)
+            loss = loss_fn(y_pred, y)
             test_loss += loss.item()
 
-            y_pred_labels = y_pred_logits.argmax(dim=1)
-            test_acc += ((y_pred_labels == y).sum().item() / len(y_pred_labels))
+            y_pred_labels = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+            test_acc += accuracy_fn(y, y_pred_labels)
 
     test_loss = test_loss / len(dataloader)
     test_acc = test_acc / len(dataloader)
@@ -77,21 +84,20 @@ def train(model: torch.nn.Module,
             optimizer=optimizer,
             device=device
         )
+        test_loss, test_acc = test_step(
+            model=model,
+            dataloader=test_dataloader,
+            loss_fn=loss_fn,
+            device=device
+        )
         print(
             f'Epoch: {epoch + 1} '
             f'train loss: {train_loss:.4f} |'
         )
         results['train_loss'].append(train_loss)
         results['train_acc'].append(train_acc)
-
-    test_loss, test_acc = test_step(
-        model=model,
-        dataloader=test_dataloader,
-        loss_fn=loss_fn,
-        device=device
-    )
-    results['test_loss'].append(test_loss)
-    results['test_acc'].append(test_acc)
+        results['test_loss'].append(test_loss)
+        results['test_acc'].append(test_acc)
 
     return results
 

model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
-
+from utils import device
 
 class VanillaRNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -26,17 +26,16 @@ class VanillaRNN(nn.Module):
         :return outputs: List of output vectors
                       h: Final hidden state
         """
+        batch_size = inputs.size(0)
+
         if h_prev is None:
-            h_prev = torch.zeros(self.hidden_size, self.output_size)
+            h_prev = torch.zeros(batch_size, self.hidden_size).to(inputs.device)
 
         h = h_prev
-        outputs = []
 
-        for x in inputs:
-            h = torch.tanh(torch.matmul(self.Wxh, x) + torch.matmul(self.Whh, h) + self.bh)
-            y = torch.matmul(self.Why, h) + self.by
-            outputs.append(y)
-        return outputs, h
+        h = torch.tanh(torch.matmul(inputs, self.Wxh.t()) + torch.matmul(h, self.Whh.t()) + self.bh.t())
+        y = torch.matmul(h, self.Why.t() + self.by.t())
+        return y
 
 
 if __name__ == '__main__':